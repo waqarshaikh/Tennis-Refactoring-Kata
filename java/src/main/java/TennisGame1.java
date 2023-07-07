ffec13fa1f1a627e8c18e99c8601b5e40efa28f7
Waqar <waqar@incubyte.co>
2023-07-07 12:14:42 +0530

    make the fields final

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -3,8 +3,8 @@ public class TennisGame1 implements TennisGame {
     
     private int player1Score = 0;
     private int player2Score = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
