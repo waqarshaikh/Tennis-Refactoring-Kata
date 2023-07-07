59c49df2e3dbc48b8ca7b270042763f8fb673349
Waqar <waqar@incubyte.co>
2023-07-07 12:16:00 +0530

    replace `==` with `isEqual()` method

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
     
@@ -12,7 +13,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (Objects.equals(playerName, "player1"))
             player1Score += 1;
         else
             player2Score += 1;
